85e48e26a09e510ce11b8f0acc1284cdcee0a359
Jonathan Perry <jonathanperry@sequencemedia.net>
2021-12-29 00:45:01 +0000

    Common

--- build/gulp/post-commit.mjs
+++ build/gulp/post-commit.mjs
@@ -1,41 +1,14 @@
 import debug from 'debug'
 
 import {
-  readFile
-} from 'fs/promises'
-
-import {
-  exec
-} from 'child_process'
-
-const OPTIONS = {
-  maxBuffer: 1024 * 500
-}
+  getPackageVersion,
+  gitTag
+} from '#build/gulp/common'
 
 const log = debug('@sequencemedia/music-library-parser:build:gulp:post-commit')
 const error = debug('@sequencemedia/music-library-parser:build:gulp:post-commit:error')
 
 log('`@sequencemedia/music-library-parser` is awake')
-
-export async function getPackageVersion () {
-  log('getPackageVersion')
-
-  const {
-    version = '0.0.0'
-  } = JSON.parse(await readFile('./package.json', 'utf8'))
-
-  return version
-}
-
-export function gitTag (a = '0.0.0', m = `v${a}`) {
-  log('gitTag')
-
-  return (
-    new Promise((resolve, reject) => {
-      exec(`git tag -a v${a} -m "${m}"`, OPTIONS, (e, v) => (!e) ? resolve(v) : reject(e))
-    })
-  )
-}
 
 export default async function postCommit () {
   log('post-commit')
--- build/gulp/pre-commit.mjs
+++ build/gulp/pre-commit.mjs
@@ -1,122 +1,18 @@
 import debug from 'debug'
 
 import {
-  exec
-} from 'child_process'
+  notStagedChanges,
+  hasStagedChanges,
+  getGitRemoteShowOriginHeadBranch,
+  notPackageVersionChanges,
+  patchPackageVersion,
+  addPackageVersionChanges
+} from '#build/gulp/common'
 
 const log = debug('@sequencemedia/music-library-parser:build:gulp:pre-commit')
+const error = debug('@sequencemedia/music-library-parser:build:gulp:pre-commit:error')
 
 log('`@sequencemedia/music-library-parser` is awake')
-
-const PACKAGE_VERSION_CHANGES = /\-+\s+"version":\s+"(\d+\.\d+\.\d+)",+\s+\++\s+"version":\s+"(\d+\.\d+\.\d+)",+\s+/ /* eslint-disable-line no-useless-escape */
-
-const HAS_STAGED_CHANGES = /Changes to be committed/s
-
-const NOT_STAGED_CHANGES = /Changes not staged for commit/s
-
-const OPTIONS = {
-  maxBuffer: 1024 * 500
-}
-
-const BRANCH = 'master'
-
-const trim = (v) => v.split('\n').map((v) => v.trimEnd()).join('\n').trim()
-
-function use (key) {
-  const log = debug(`@sequencemedia/music-library-parser:${key}`)
-
-  return function use (v) {
-    log(trim(v))
-  }
-}
-
-export function getGitRemoteShowOriginHeadBranch () {
-  log('getGitRemoteShowOriginHeadBranch')
-
-  return (
-    new Promise((resolve, reject) => {
-      const command = 'git remote show origin | awk \'/HEAD branch/ {print $NF}\''
-
-      const {
-        stdout,
-        stderr
-      } = exec(command, OPTIONS, (e, v = '') => (!e) ? resolve(trim(v)) : reject(e))
-
-      stdout.on('data', use('git-remote-show-origin-head-branch'))
-      stderr.on('data', use('git-remote-show-origin-head-branch'))
-    })
-  )
-}
-
-export function hasPackageVersionChanges (branch = BRANCH) {
-  log('hasPackageVersionChanges')
-
-  return (
-    new Promise((resolve, reject) => {
-      exec(`git diff HEAD origin/${branch} package.json`, OPTIONS, (e, v) => (!e) ? resolve(PACKAGE_VERSION_CHANGES.test(v)) : reject(e))
-    })
-  )
-}
-
-export function notPackageVersionChanges (branch = BRANCH) {
-  log('notPackageVersionChanges')
-
-  return (
-    new Promise((resolve, reject) => {
-      exec(`git diff HEAD origin/${branch} package.json`, OPTIONS, (e, v) => (!e) ? resolve(PACKAGE_VERSION_CHANGES.test(v) !== true) : reject(e))
-    })
-  )
-}
-
-export function hasStagedChanges () {
-  log('hasStagedChanges')
-
-  return (
-    new Promise((resolve, reject) => {
-      exec('git status', OPTIONS, (e, v) => (!e) ? resolve(HAS_STAGED_CHANGES.test(v)) : reject(e))
-    })
-  )
-}
-
-export function notStagedChanges () {
-  log('notStagedChanges')
-
-  return (
-    new Promise((resolve, reject) => {
-      exec('git status', OPTIONS, (e, v) => (!e) ? resolve(NOT_STAGED_CHANGES.test(v)) : reject(e))
-    })
-  )
-}
-
-export function notPushedChanges (branch = BRANCH) {
-  log('notPushedChanges')
-
-  return (
-    new Promise((resolve, reject) => {
-      exec(`git log origin/${branch}..HEAD`, OPTIONS, (e, v) => (!e) ? resolve(!!v) : reject(e))
-    })
-  )
-}
-
-export function patchPackageVersion () {
-  log('patchPackageVersion')
-
-  return (
-    new Promise((resolve, reject) => {
-      exec('npm version patch -m %s -n --no-git-tag-version --no-verify', OPTIONS, (e) => (!e) ? resolve() : reject(e))
-    })
-  )
-}
-
-export function addPackageVersionChanges () {
-  log('addPackageVersionChanges')
-
-  return (
-    new Promise((resolve, reject) => {
-      exec('git add package.json package-lock.json', OPTIONS, (e) => (!e) ? resolve() : reject(e))
-    })
-  )
-}
 
 export default async function preCommit () {
   log('pre-commit')
@@ -139,6 +35,6 @@ export default async function preCommit () {
       }
     }
   } catch ({ code = 'NONE', message = 'No error message defined' }) {
-    log({ code, message })
+    error({ code, message })
   }
 }
