885a81d21f865486a27cca742b599b01193464a3
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-04-11 19:03:31 +0100

    `rimraf`

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,18 +1,13 @@
 import {
-  resolve
+  resolve,
+  join
 } from 'node:path'
 import {
   rimraf
 } from 'rimraf'
 
-export function clear (destination = './Music Library') {
-  const d = resolve(destination)
-
-  return (
-    new Promise((resolve, reject) => {
-      rimraf(`${d}/*`, (e) => (!e) ? resolve() : reject(e))
-    })
-  )
-}
+export const clear = (destination = './Music Library') => (
+  rimraf(join(resolve(destination), '*'))
+)
 
 export * as library from '#music-library-parser/library'
